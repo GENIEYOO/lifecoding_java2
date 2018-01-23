9479c4c1dbcbea1c744b8bf3bbe9070721de967f
GENIEYOO <forstudym.m@gmail.com>
2018-01-23 21:23:11 +0900

    calculatordemo added

---
+++ src/org/opentutorials/javatutorials/object/ClaculatorDemo3.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.object;
+
+//중복을 최대한 제거하려고
+public class ClaculatorDemo3 {
+	public static void avg(int left, int right) {
+		System.out.println((left + right)/2);
+	}
+
+	public static void sum(int left, int right){
+	System.out.println(left+right);
+	}
+
+}
+//서로 연관되어있는 데이터와 연산을 최대한 그룹핑
